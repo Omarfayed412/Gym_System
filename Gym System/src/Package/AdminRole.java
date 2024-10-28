e7d3ee80c3351a4b02239fa6c19f4431c7b2d198
jomana <jomanaehab666@yahoo.com>
2024-10-29 02:33:51 +0300

    انا تعبت

--- Gym System/src/Package/AdminRole.java
+++ Gym System/src/Package/AdminRole.java
@@ -5,7 +5,8 @@
 package Package;
 
 import java.util.*;
-
+import java.io.*;
+import java.lang.System;
 /**
  *
  * @author 20112
@@ -14,23 +15,40 @@ public class AdminRole{
     private TrainerDatabase database;
 
     public AdminRole() {
-        this.database = database;
+        this.database = new TrainerDatabase("Trainers.txt");
+        database.readFromFile();
     }
     
     
     public void addTrainer(String trainerID, String name, String email, String speciality, String phoneNum) {
+        if (!database.contains(trainerID)) {
+            Trainer newTrainer = new Trainer(trainerID, name, email, speciality, phoneNum);
+            database.insertRecord(newTrainer);
+            System.out.println("Trainer added successfully.");
+        } else {
+            System.out.println("Trainer already exists.");
+        }
+      
     
     }
     
-    public ArrayList<Trainer> getListOfTrainers() {
-    
+    public List<Trainer> getListOfTrainers() {
+        return database.returnAllRecords();
     }
     
     public boolean removeTrainer(String ID) {
-    
+        if (database.contains(ID)) {
+            database.deleteRecord(ID);
+            System.out.println("Trainer has been removed.");
+            return true;
+        } else {
+            System.out.println("Trainer not found.");
+            return false;
+        }
     }
     
     public void logout() {
-    
+        database.saveToFile();
+        System.out.println("Saved and logged out.");
     }
 }
