5d5b305f958a8c5fb67342d53bc8540493883089
jomana <jomanaehab666@yahoo.com>
2024-11-05 14:51:47 +0200

    FirstPage

--- Gym System/src/Backend/Main.java
+++ Gym System/src/Backend/Main.java
@@ -2,11 +2,13 @@ package Backend;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import Frontend.FirstPageWindow;
+
 
 public class Main {
 
     public static void main(String[] args) {
-        new FirstPage();
+        new FirstPageWindow();
 
     }
 }
